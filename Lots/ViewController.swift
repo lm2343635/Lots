780169a1628e39b90c3a5ffd7889fd35d0d80c93
limeng <lm2343635@126.com>
2017-01-26 13:13:44 +0900

    Initial Commit

---
+++ Lots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lots
+//
+//  Created by lidaye on 26/01/2017.
+//  Copyright © 2017 fczm.pw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
